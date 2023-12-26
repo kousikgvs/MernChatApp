8701d5d115dc54b4f40531398537c0038be07d6c
kousikgvs <kousikgvs.com>
2023-12-26 13:01:08 +0530

    Added Routes for Adding and Removing people from Group

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -13,6 +13,10 @@ const chatModel = mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Message",
   },
+  groupAdmin: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"User",
+  }
 }, {
   timestamps: true,
 });
